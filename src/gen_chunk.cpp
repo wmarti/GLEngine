d73676f50e5f8b799c09ca9416e0466893ace0a6
Will Martin <wmarti@bu.edu>
2022-06-09 15:05:10 -0400

    Created 16x16x16 chunk.

--- src/gen_chunk.cpp
+++ src/gen_chunk.cpp
@@ -6,14 +6,16 @@ void Chunk::gen_flat_16_16(Block& block, Shader& shader) {
 
     for (uint32_t row = 1; row < 17; row++) {
         for (uint32_t col = 1; col < 17; col++) {
-            model = glm::mat4(1.0f);
-            model = glm::translate(model, glm::vec3((float)col-8, 0.0f, (float)row));
-            model = glm::rotate(
-                model,
-                glm::radians(90.0f),
-                glm::vec3(1.0f, 0.0f, 0.0f));
-            shader.setMat4("model", model);
-            block.draw();
+            for (uint32_t aisles = 1; aisles < 17; aisles++) {
+                model = glm::mat4(1.0f);
+                model = glm::translate(model, glm::vec3((float)col - 8, (float)aisles-16, (float)row));
+                model = glm::rotate(
+                    model,
+                    glm::radians(90.0f),
+                    glm::vec3(1.0f, 0.0f, 0.0f));
+                shader.setMat4("model", model);
+                block.draw();
+            }
         }
     }
 }
